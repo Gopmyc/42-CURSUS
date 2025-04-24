874e57643522e7ae8f2bc7499a27d445e7b1910f
Gopmyc <74107065+Gopmyc@users.noreply.github.com>
2025-04-25 01:19:37 +0200

    git commit -m "[MAIN]42 > philo > 'Sanithizer clean, project functional, just make refactoring and it's clear'"

---
+++ philo/srcs/activity.c
@@ -0,0 +1,73 @@
+#include "../include/philo.h"
+
+void	write_status(char *str, t_philo *ph)
+{
+	long int	time;
+
+	time = -1;
+	time = actual_time() - ph->pa->start_t;
+	if (time >= 0 && time <= 2147483647 && !check_death(ph, 0))
+	{
+		printf("%ld ", time);
+		printf("Philo %d %s", ph->id, str);
+	}
+}
+
+void	sleep_think(t_philo *ph)
+{
+	pthread_mutex_lock(&ph->pa->write_mutex);
+	write_status("is sleeping\n", ph);
+	pthread_mutex_unlock(&ph->pa->write_mutex);
+	ft_usleep(ph->pa->sleep);
+	pthread_mutex_lock(&ph->pa->write_mutex);
+	write_status("is thinking\n", ph);
+	pthread_mutex_unlock(&ph->pa->write_mutex);
+}
+
+void	activity(t_philo *ph)
+{
+	pthread_mutex_t	*first;
+	pthread_mutex_t	*second;
+
+	if (&ph->l_f < ph->r_f)
+	{
+		first = &ph->l_f;
+		second = ph->r_f;
+	}
+	else
+	{
+		first = ph->r_f;
+		second = &ph->l_f;
+	}
+
+	pthread_mutex_lock(first);
+	pthread_mutex_lock(&ph->pa->write_mutex);
+	write_status("has taken a fork\n", ph);
+	pthread_mutex_unlock(&ph->pa->write_mutex);
+
+	if (!ph->r_f)
+	{
+		ft_usleep(ph->pa->die * 2);
+		pthread_mutex_unlock(first);
+		return ;
+	}
+
+	pthread_mutex_lock(second);
+	pthread_mutex_lock(&ph->pa->write_mutex);
+	write_status("has taken a fork\n", ph);
+	pthread_mutex_unlock(&ph->pa->write_mutex);
+
+	pthread_mutex_lock(&ph->pa->write_mutex);
+	write_status("is eating\n", ph);
+	pthread_mutex_unlock(&ph->pa->write_mutex);
+	pthread_mutex_lock(&ph->pa->time_eat);
+	ph->ms_eat = actual_time();
+	pthread_mutex_unlock(&ph->pa->time_eat);
+
+	ft_usleep(ph->pa->eat);
+
+	pthread_mutex_unlock(second);
+	pthread_mutex_unlock(first);
+
+	sleep_think(ph);
+}
---
+++ philo/srcs/threads.c
@@ -0,0 +1,71 @@
+#include "../include/philo.h"
+
+void	*is_dead(void *data)
+{
+	t_philo	*ph;
+
+	ph = (t_philo *)data;
+	while (!check_death(ph, 0))
+	{
+		pthread_mutex_lock(&ph->pa->time_eat);
+		pthread_mutex_lock(&ph->pa->finish);
+		if (!ph->finish && ((actual_time() - ph->ms_eat) >= (long)(ph->pa->die)))
+		{
+			pthread_mutex_unlock(&ph->pa->finish);
+			pthread_mutex_unlock(&ph->pa->time_eat);
+
+			pthread_mutex_lock(&ph->pa->write_mutex);
+			write_status("died\n", ph);
+			pthread_mutex_unlock(&ph->pa->write_mutex);
+
+			check_death(ph, 1);
+			break ;
+		}
+		pthread_mutex_unlock(&ph->pa->time_eat);
+		pthread_mutex_unlock(&ph->pa->finish);
+		usleep(100);
+	}
+	return (NULL);
+}
+
+void	*thread(void *data)
+{
+	t_philo	*ph;
+
+	ph = (t_philo *)data;
+	if (ph->id % 2 == 0)
+		ft_usleep(ph->pa->eat / 10);
+	while (!check_death(ph, 0))
+	{
+		activity(ph);
+		if ((int)++ph->nb_eat == ph->pa->m_eat)
+		{
+			pthread_mutex_lock(&ph->pa->finish);
+			ph->finish = 1;
+			ph->pa->nb_p_finish++;
+			if (ph->pa->nb_p_finish == ph->pa->total)
+			{
+				pthread_mutex_unlock(&ph->pa->finish);
+				check_death(ph, 2);
+			}
+			else
+				pthread_mutex_unlock(&ph->pa->finish);
+			return (NULL);
+		}
+	}
+	return (NULL);
+}
+
+int	threading(t_p *p)
+{
+	int	i = 0;
+
+	while (i < p->a.total)
+	{
+		p->ph[i].pa = &p->a;
+		if (pthread_create(&p->ph[i].thread_id, NULL, thread, &p->ph[i]) != 0)
+			return (ft_exit("Error creating main thread\n"));
+		i++;
+	}
+	return (1);
+}
